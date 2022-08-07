6c3dff214e04594a69bb5b0f9d739c6341b9b5b4
Juliapirat <109736239+Juliapirat@users.noreply.github.com>
2022-08-07 16:47:19 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React, { useState, useLayoutEffect } from "react";
+import { render } from "react-dom";
+import mapboxgl from "mapbox-gl";
+import "./styles.css";
+
+function App() {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiYm9vYW5kcmV3IiwiYSI6ImNrd3M2ZGF1YzBhcDEyb21obzUwcDlvNXMifQ.q0NAPJB4RMgRRI8Fi9PWZg";
+
+  const [marker, setMarker] = useState([]);
+  const [shopname, setShopname] = useState("");
+
+  useLayoutEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/dark-v10",
+      center: [37.610641, 55.761994],
+      zoom: 10
+    });
+    const marker = new mapboxgl.Marker()
+      .setLngLat([37.6173, 55.7558])
+      .addTo(map);
+    setMarker(marker);
+  }, []);
+
+  const stores = {
+    km20: [37.610641, 55.761994],
+    belief: [37.601152, 55.733396],
+    brandshop: [37.616812, 55.767839]
+  };
+
+  function HandleonChange(e) {
+    setShopname(e.target.value);
+    marker.setLngLat(stores[e.target.value]);
+  }
+
+  return (
+    <>
+      <div className="map-overlay">
+        <h3>Выберите магазин: </h3>
+        <select value={shopname} onChange={HandleonChange}>
+          <option value="km20">KM20</option>
+          <option value="belief">BELIEF</option>
+          <option value="brandshop">BRANDSHOP</option>
+        </select>
+      </div>
+      <div id="map"></div>
+    </>
+  );
+}
+
+render(<App />, document.querySelector("#root"));
